7adc2110f350e8fb46a91f94b708149eb2647b19
elliot <1094253525@qq.com>
2017-08-21 14:35:02 +0800

    fix some problem

--- route/myConnection.js
+++ route/myConnection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'localhost',
+  host            : 'rm-wz952uk54ol0wzj43o.mysql.rds.aliyuncs.com',
   port            : '3306',
   user            : 'root',
-  password        : '0000',
+  password        : 'Xl1101000',
   database        : 'website'
 });
 
